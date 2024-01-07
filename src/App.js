09daccb89da3e78815997f9d1f2ed3560e60c515
SergioVed <vedankinsergij@gmail.com>
2024-01-07 20:50:05 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import { useState, useEffect } from 'react';
+// function App() {
+//   const [selectValue, setSelectValue] = useState("alphabetical")
+//   const [selectValue1, setSelectValue1] = useState("pc")
+//   const [games, setGames] = useState([])
+//   useEffect(() => {
+//     let data = async(url) => {
+//     let responce = await fetch(url, {
+//       headers: {
+//         'X-RapidAPI-Key': '4315a56593msh57fafa17a112819p155efbjsn46ef0b5e1a69',
+//         'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+//       }
+//     })
+//     let data = await responce.json()
+//     console.log(data)
+//     setGames(data)
+//   }
+//   data(`https://free-to-play-games-database.p.rapidapi.com/api/games?platform=${selectValue1}&sort-by=${selectValue}`)}, [selectValue, selectValue1])
+//   return (
+//     <div className="App">
+//       <div>
+//         <select onChange={(event) => setSelectValue(event.target.value)}>
+//           <option value={"alphabetical"}>Alphabetical</option>
+//           <option value={"popularity"}>Popularity</option>
+//           <option value={"release-date"}>Release-date</option>
+//         </select>
+//         <select onChange={(event) => setSelectValue1(event.target.value)}>
+//           <option value={"pc"}>pc</option>
+//           <option value={"browser"}>browser</option>
+//         </select>
+//         {games && games.map((game) => (
+//           <div>{game && <div>{game.title}: {game.platform}</div>}</div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+// export default App;
---
+++ src/components/homePage/index.jsx
@@ -0,0 +1,12 @@
+
+export const HomePage = () => {
+
+    return(
+        <>
+            <div>
+                <h1>FREE TO GAME</h1>
+                <h4></h4>
+            </div>
+        </>
+    )
+}
